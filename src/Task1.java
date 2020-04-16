081c99b2db4ea7dbe6b07a385483fc48b36d9fcd
Ilya Posov <iposov@gmail.com>
2020-04-16 12:01:45 +0300

    Изменили решение Task 1

--- src/Task1.java
+++ src/Task1.java
@@ -1,7 +1,8 @@
 public class Task1 {
 
+    //новый комментарий
     public static void main(String[] args) {
-        System.out.println("task 1");
+        System.out.println("task 1!");
     }
 
 }
