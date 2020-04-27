346bdde279f2bddb89875ddb159da2ab9d72854a
Ilya Posov <iposov@gmail.com>
2020-04-27 14:55:54 +0300

    улучшил задачу 1 в университете

--- src/Task1.java
+++ src/Task1.java
@@ -3,6 +3,7 @@ public class Task1 {
     //новый комментарий
     public static void main(String[] args) {
         System.out.println("task 1!");
+        // какое-то изменение, сделанное в Университете
     }
 
 }
